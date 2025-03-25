8935095df6ebe17c0f4efefd1a7b78843a06ed78
boeses-genie <k.kharlamow@gmail.com>
2025-03-25 12:51:49 +0500

    deploy web hook

---
+++ webhook_deploy.py
@@ -0,0 +1,42 @@
+import os
+import shutil
+from subprocess import Popen, PIPE, run
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class Webhook(BaseHTTPRequestHandler):
+    def do_POST(self):
+        directory = '/path/to/base/dir/'
+        src = 'path/to/source/'
+        dest = 'path/to/destination/'
+
+        run(
+            '/usr/bin/git pull',
+            shell=True, cwd=directory + src,
+            stdin=PIPE,
+            stdout=PIPE,
+            stderr=PIPE
+        )
+
+        changed_files = Popen(
+            '/usr/bin/git diff --name-only HEAD HEAD~1',
+            shell=True,
+            cwd=directory + src,
+            stdin=PIPE,
+            stdout=PIPE,
+            stderr=PIPE
+        ).communicate()[0].strip().decode('utf-8').split('\n')
+
+        for file_name in changed_files:
+            full_file_name = os.path.join(directory + src, file_name)
+
+            if os.path.isfile(full_file_name):
+                shutil.copy(full_file_name, directory + dest)
+            elif os.path.isfile(directory + dest + file_name):
+                os.remove(directory + dest + file_name)
+
+        self.send_response(200)
+        self.end_headers()
+
+if __name__ == '__main__':
+    httpd = HTTPServer(('', 8081), Webhook)
+    httpd.serve_forever()
